53277913e39d5f3f083ac974362bbae82c69debf
Steven Armstrong <stevenedwardarmstrong@gmail.com>
2024-09-29 19:59:34 -0400

    address manifest errors

--- InterviewHelper/background.js
+++ InterviewHelper/background.js
@@ -2,6 +2,9 @@ async function open_website() {
     var newUrl = "https://google.com";
     chrome.tabs.create({url: newUrl});
 }
+async function start_listening() {
+    
+}
 chrome.commands.onCommand.addListener((command) =>{
     if (command === "test_command") {
         open_website();
